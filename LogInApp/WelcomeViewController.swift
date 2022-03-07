ff3f1b96ed04dbc8171c6e3eca95e4dd913243c9
Evgeniya Shevtsova <shevtsova-evgeniia@yandex.ru>
2022-03-08 01:18:33 +0300

    edits to storyboards

--- LogInApp/WelcomeViewController.swift
+++ LogInApp/WelcomeViewController.swift
@@ -19,6 +19,7 @@ class WelcomeViewController: UIViewController {
         gradientLayer.colors = [#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1).cgColor, #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1).cgColor]
         backgroundGradientView.layer.addSublayer(gradientLayer)
     
+        view.layer.insertSublayer(gradientLayer, at: 0)
     }
     
 
